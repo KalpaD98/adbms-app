a68dbec7dca09d86a30312f4642f3ece1b9bc865
mihinduk08 <mihinduk@sheridanc.on.ca>
2021-11-15 18:54:42 +0530

    added example attribute

--- src/main/java/com/adbms/app/models/Retailer.java
+++ src/main/java/com/adbms/app/models/Retailer.java
@@ -1,5 +1,16 @@
 package com.adbms.app.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Retailer {
+    @Id
+    @Getter
+    @Setter
+    private String id;
+
 
 }
