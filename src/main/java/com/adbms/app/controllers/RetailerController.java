2964c7973c5fc5b3841c13ef68ebe49fb399dcbf
mihinduk08 <mihinduk@sheridanc.on.ca>
2021-11-15 19:42:04 +0530

    added controller for retailer

--- src/main/java/com/adbms/app/controllers/RetailerController.java
+++ src/main/java/com/adbms/app/controllers/RetailerController.java
@@ -1,4 +1,28 @@
 package com.adbms.app.controllers;
 
+import com.adbms.app.repositories.RetailerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@Controller
+@RequestMapping(value = "/retailers")
 public class RetailerController {
+
+    private final RetailerRepository retailerRepository;
+
+    @Autowired
+    public RetailerController(RetailerRepository retailerRepository) {
+        this.retailerRepository = retailerRepository;
+    }
+
+    @GetMapping()
+    public String getAllRetailers(Model model) {
+        model.addAttribute("retailers", retailerRepository.findAll());
+        return "retailers";
+    }
 }
