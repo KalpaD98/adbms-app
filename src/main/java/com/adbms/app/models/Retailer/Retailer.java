2d45bc18cdde2e8f8cae77fd4c22bec92ca2767f
KalpaD98 <kalpafernando1998@gmail.com>
2021-11-22 00:47:04 +0530

    minor changes

--- src/main/java/com/adbms/app/models/Retailer/Retailer.java
+++ src/main/java/com/adbms/app/models/Retailer/Retailer.java
@@ -3,7 +3,6 @@ package com.adbms.app.models.Retailer;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.Email;
