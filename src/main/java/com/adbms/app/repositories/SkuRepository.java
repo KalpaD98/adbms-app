3ea67978f0aa28479f775155d42da77684c313dc
NethraGuruge <69167462+NethraGuruge@users.noreply.github.com>
2021-11-27 22:52:04 +0530

    OrderItems - frontend added, Backend issues fixed, SKU repo refactored

--- src/main/java/com/adbms/app/repositories/SkuRepository.java
+++ src/main/java/com/adbms/app/repositories/SkuRepository.java
@@ -1,8 +1,10 @@
 package com.adbms.app.repositories;
 
+import com.adbms.app.models.OrderItems.OrderItems;
 import com.adbms.app.models.Sku.Sku;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface SkuRepository extends MongoRepository<Sku, String> {
 
+    Sku getSkuByName(String name);
 }
